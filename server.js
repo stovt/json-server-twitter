f3561abe3950da97528fb17539d16ac71cef2329
Sergey Tovt <st@norse.co>
2019-09-25 14:14:51 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+
+server.use((req, res, next) => {
+  // Converts all the methods to GET and move payload to query params
+  // This way it will make JSON Server that it's GET request
+  req.method = 'GET';
+  req.query = req.body;
+  // Continue to JSON Server router
+  next();
+})
+
+server.use(jsonServer.rewriter({
+  '/tweet/get/all': '/tweets/',
+  '/users/get/all': '/users/',
+  '/users/get/:id': '/usersById/:id',
+  '/auth/signin': '/signin/',
+  '/auth/signup': '/signup/',
+  '/tweet/get/user/:id': '/tweetsById/:id',
+  '/tweet/get/parent/:id': '/comments/:id',
+  '/tweet/delete/:id': '/tweetDelete/',
+  '/tweet/create': '/errorMessage/',
+  '/like/:id': '/errorMessage/'
+}))
+
+server.use(router);
+
+server.listen(port, () => {
+  console.log('JSON Server is running')
+});
