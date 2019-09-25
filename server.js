d8e78757d1976d9b9c2a09ea3bba33bd2ffd9300
Sergey Tovt <st@norse.co>
2019-09-25 14:21:43 +0300

    fix in server.js

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 
-server.use((req, res, next) => {
-  // Converts all the methods to GET and move payload to query params
-  // This way it will make JSON Server that it's GET request
-  req.method = 'GET';
-  req.query = req.body;
+server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    req.method = 'GET'
+    req.query = req.body
+  }
   // Continue to JSON Server router
-  next();
+  next()
 })
 
 server.use(jsonServer.rewriter({
